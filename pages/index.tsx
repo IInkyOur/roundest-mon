b4d2e4c78b22a3c5e95e42f07912eca6ca0ce3db
Yukinori Eguchi <yukinori.eguchi@gmail.com>
2022-07-24 12:07:51 +0900

    basic UI structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-  return <div className=''>Hello world</div>;
+  return (
+    <div className='h-screen w-screen flex flex-col justify-center items-center'>
+      <div className='text-2xl text-center'>Which Pokemon is Rounder?</div>
+      <div className='p-2' />
+      <div className='border rounded p-8 flex items-center justify-between max-w-2xl'>
+        <div className='w-16 h-16 bg-red-200' />
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-red-200' />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
